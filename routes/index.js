7208a481866662d0e3eb9434f50d434613935149
StupFX <rudy.devisscher@gmail.com>
2015-12-08 13:55:09 +0100

    Add .jade files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mixtour Event - Accueil' });
+});
+
+router.get('/login-registration', function(req, res, next) {
+  res.render('login-registration', { title: 'Mixtour Event - Inscription / Connexion' });
+});
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq', { title: 'Mixtour Event - Règles / FAQ' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Mixtour Event - Règles / FAQ' });
+});
+
+router.get('/game', function(req, res, next) {
+  res.render('game', { title: 'Mixtour Event - Règles / FAQ' });
 });
 
 module.exports = router;
